d8b053506dbdbb14c61c2c2f1092a5865ec5a2eb
Gusts Baltaisbrencis <112851339+gustsbb@users.noreply.github.com>
2022-09-19 10:46:42 +0300

    Add files via upload

---
+++ paldies vai nepareizi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+    int main () {
+
+
+    int x;
+            cout <<"Ievadi skaitli no 10 lidz 20!"<<endl ;
+
+    cin>>x;
+    if(10<= x && x <=20) {
+        printf("pareizi");
+    } else{
+    printf("nepareizi");
+    }
+
+
+
+
+
+
+
+    }
